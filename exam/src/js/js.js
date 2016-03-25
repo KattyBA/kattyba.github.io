a76a6e6f47b612c0397771c744c619110dc23155
kattyBA <kat_B_@mail.ru>
2016-03-25 22:12:54 +0200

    add ie html

--- exam/src/js/js.js
+++ exam/src/js/js.js
@@ -3,38 +3,41 @@ $(function() {
     function getImagesByRequest(phrase) {
 
         $.ajax({
-        url: 'http://api.pixplorer.co.uk/image?word='+ phrase +'&amount=7&size=l',
-        type: "GET",
-        dataType: "json",
-        cache: false,
-        success: function(data){
-            $(".grid").remove();
-            var piclist = tmpl($('#activity__template').html(), {data: data});
-            $('.activity .wrapper').append(piclist);
+            url: 'https://pixabay.com/api/?key=2271973-553054d4a3087a5229bd26af2&q=' + phrase + '&orientation=horizontal&image_type=photo&min_width=410&min_height=620&per_page=7',
+            type: 'GET',
+            dataType: 'json',
+            cache: false,
+            success: function(data){
+                if (data.hits) {
+                    $(".grid").remove();
+                    var piclist = tmpl($('#activity__template').html(), {data: data});
+                    $('.activity .wrapper').append(piclist);
 
-            $('.grid').isotope({
-                itemSelector: '.grid-item',
-                layoutMode: 'packery',
-                packery: {
-                    gutter: 0
+                    $('.grid').isotope({
+                        itemSelector: '.grid-item',
+                        layoutMode: 'packery',
+                        packery: {
+                            gutter: 0
+                        }
+                    });
+
+                } else {
+                    $('.activity .wrapper').append("<p>Sorry. Something on the server has crashed</p>");
                 }
-            });
+            }
+        });
 
-        }
-    });
+    }
 
-}
-
-
-
-getImagesByRequest();
-var buttonSearch = $('.search__button');
-
-$('.search__button').click(function(e) {
-    e.preventDefault();
-    var searchField = $(".input__form").val();
-    getImagesByRequest(searchField);
-})
+    var inputWords = ['sun', 'snow', 'snowboard', 'flowers', 'forest', 'fishing', 'sea', 'mountain'];
+    var randomWords = Math.floor(Math.random() * inputWords.length);
+    getImagesByRequest(inputWords[randomWords]);
+    var buttonSearch = $('.search__button');
+    $('.search__button').click(function(e) {
+        e.preventDefault();
+        var searchField = $(".input__form").val();
+        getImagesByRequest(searchField);
+    })
 
             $('.jcarousel').jcarousel({
     			animation: 'slow',
